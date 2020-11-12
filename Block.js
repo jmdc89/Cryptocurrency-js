a904f167ddb67698cb1275c7f332cdbd3842b3a3
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2020-11-12 19:49:11 +0100

    calcularHash
    Falta añadir el SHA256

--- Block.js
+++ Block.js
@@ -1,9 +1,29 @@
+
+
+
+const SHA256 = require('crypto-js/sha256');
+
+
+
 class Block{
     constructor(timestamp, data, previousHash = ''){
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
+        this.hash = this.calcularHash();
     }
 
-    
+    calcularHash(){
+        return SHA256(this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+
+
+
+
+
+
+
 }
+
+
+module.exports = Block;
---
+++ index.js
@@ -0,0 +1,5 @@
+const Block = require('./Block');
+
+const bloquePrueba = new Block('12/11/2020', 'Bloque de prueba', '0');
+
+console.log(bloquePrueba);
