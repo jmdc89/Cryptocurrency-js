304972b36d1d702a9da48b7f9a9010b74940f4da
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2020-11-12 20:03:12 +0100

    obtenerbloque + ultimo bloque

--- Blockchain.js
+++ Blockchain.js
@@ -13,6 +13,16 @@ class Blockchain {
     crearBloqueGenesis(){
         return new Block('01/01/2020', 'Bloque Genesis', '0');
     }
+
+    obtenerUltimoBloque(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    agregarBloque(newBlock){
+        newBlock.previousHash = this.obtenerUltimoBloque().hash;
+        newBlock.hash = newBlock.calcularHash();
+        this.chain.push(newBlock);
+    }
 }
 
 module.exports = Blockchain;
